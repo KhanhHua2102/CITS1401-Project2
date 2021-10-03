15e473d0fc77d9baacb8f4941ed82fcd9f02b408
Quang Khanh Hua <khanhhua@comscitech.onmicrosoft.com>
2021-10-03 09:15:38 +0700

    update

---
+++ 22928469.py
@@ -0,0 +1,27 @@
+"""
+This is a computer program that can read the data from a CSV (comma-separated values)
+file provided to it and return different interesting analytical results.
+Author: Khanh Hua Quang
+Student ID: 22928469
+"""
+
+# IMPORTANT: Round results to 4 decimal
+
+def LDCount():
+    return
+
+def simScore():
+    return
+
+def DCommon():
+    return
+
+def LDClose():
+    return
+
+def main(inputFile,[locId1,locId2],radius):
+    return
+
+
+
+# LDCount, simScore, DCommon, LDClose = main("Locations.csv", ["L26", "L52"], 3.5)
