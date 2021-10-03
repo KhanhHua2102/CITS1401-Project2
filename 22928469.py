69626092317912a7b305d29bd0814e4f77072fa5
Quang Khanh Hua <khanhhua@comscitech.onmicrosoft.com>
2021-10-03 16:53:19 +0700

    LDCountFunc

--- 22928469.py
+++ 22928469.py
@@ -5,24 +5,91 @@ Author: Khanh Hua Quang
 Student ID: 22928469
 """
 
-# IMPORTANT: Round results to 4 decimal
-
 def readFile(inputFile):
     try:
         with open (inputFile, "r") as file:
-            header = file.readline().split(",")
+            header = file.readline()[:-1].split(",")
             temp = file.readlines()
             locationList = []
             for line in temp:
-                locationList.append(line[:-2].split(","))
+                locationList.append(line[:-1].split(","))
     except EOFError:
         return
+    return header, locationList
+
+def header(header):
+    # define column position in case of random header
+    headerPos = []
+    for index in range(6):
+        if header[index].lower() == "locid":
+            headerPos.append(index)
+    for index in range(6):
+        if header[index].lower() == "latitude":
+            headerPos.append(index)
+    for index in range(6):
+        if header[index].lower() == "longitude":
+            headerPos.append(index)
+    for index in range(6):
+        if header[index].lower() == "category":
+            headerPos.append(index)
+    for index in range(6):
+        if header[index].lower() == "reviews":
+            headerPos.append(index)
+    for index in range(6):
+        if header[index].lower() == "rankreview":
+            headerPos.append(index)
+    return headerPos[0], headerPos[1], headerPos[2], headerPos[3], headerPos[4], headerPos[5]
+
+# compare the locId input with locId in locationList to add x, y, category element of that locId to a list
+def element(locId, inputFile):
+    headerPos = header(readFile(inputFile)[0])
+    locationList = readFile(inputFile)[1]
+    locIdPos = headerPos[0]
+    xPos = headerPos[1]
+    yPos = headerPos[2] 
+    categoryPos = headerPos[3] 
+    reviewsPos = headerPos[4] 
+    rankPos = headerPos[5]
+    outputList = []
+    for location in locationList:
+        if locId.lower() == location[locIdPos].lower():
+            try:
+                outputList.append(float(location[xPos]))
+                outputList.append(float(location[yPos]))
+            except:
+                continue
+            outputList.append(location[categoryPos].lower())
+            try:
+                outputList.append(float(location[reviewsPos]))
+                outputList.append(float(location[rankPos]))
+            except:
+                continue
+    return outputList
+
+def distance(x1, y1, x2, y2):
+    return round(((((x2 - x1)**2) + (y2 - y1)**2)) ** (1/2), 4)
 
 def isInRadius(x1, y1, x2, y2, radius):
-    return x1 < x2 < x1 + radius and y1 < y2 < y1 + radius
+    return distance(x1, y1, x2, y2) < radius
 
-def LDCount():
-    return
+def LDCountFunc(inputFile, queryLocId, radius):
+    LDCount = [{'P': 0, 'H': 0, 'R': 0, 'C': 0, 'S': 0}, {'P': 0, 'H': 0, 'R': 0, 'C': 0, 'S': 0}]
+    for location in readFile(inputFile)[1]:
+        radius = float(radius)
+        x2 = float(location[1])
+        y2 = float(location[2])
+        
+        x1 = float(element(queryLocId[0], inputFile)[0])
+        y1 = float(element(queryLocId[0], inputFile)[1])
+        if isInRadius(x1, y1, x2, y2, radius):
+            LDCount[0][location[3]] = LDCount[0].get(location[3]) + 1
+
+        x1 = float(element(queryLocId[1], inputFile)[0])
+        y1 = float(element(queryLocId[1], inputFile)[1])
+        if isInRadius(x1, y1, x2, y2, radius):
+            LDCount[1][location[3]] = LDCount[1].get(location[3]) + 1
+        
+    return LDCount
 
 def similarity(A, B):
     numerator = (A.get("P")*B.get("P")) + (A.get("H")*B.get("H")) + (A.get("R")*B.get("R")) + (A.get("C")*B.get("C")) + (A.get("S")*B.get("S"))
@@ -31,28 +98,27 @@ def similarity(A, B):
     result = round(numerator / (denominator1 * denominator2), 4)
     return result
 
-def simScore():
+def simScoreFunc():
     return
 
-def DCommon():
+def DCommonFunc():
     return
 
-def distance(x1, y1, x2, y2):
-    return round(((((x2 - x1)**2) + (y2 - y1)**2)) ** (1/2), 4)
-
-def LDClose():
+def LDCloseFunc():
     return
 
 
-def main(inputFile, locId, radius):
-    readFile(inputFile)
-    
-    return
+def main(inputFile, querylocId, radius):
+    return LDCountFunc(inputFile, querylocId, radius)
 
 main("Locations.csv", ["L26", "L52"], 3.5)
 
-# LDCount, simScore, DCommon, LDClose = main("Locations.csv", ["L26", "L52"], 3.5)
-# print(LDCount)
+
+# IMPORTANT: Round results to 4 decimal, header name variation, matching locId
+# NEED TO REMOVE 
+LDCount = main("Locations.csv", ["L26", "L52"], 3.5)
+print(LDCount)
 # print(simScore)
 # print(DCommon)
 # print(LDClose)
+# NEED TO REMOVE 
--- project1.py
+++ project1.py
@@ -10,69 +10,81 @@ def main(inputFile,queryLocId,d1,d2):
     
     # open input file in read mode and append data into a list
     with open(inputFile, "r") as file:
-        header = file.readline().split(",")          
+        header = file.readline()[:-1].split(",")          
         temp = file.readlines()
-        locationListTemp = []
         locationList = []
         for line in temp:
             locationList.append(line[:-1].split(","))
 
-    # define column position
-    locIdPos = 0
-    xPos = 1
-    yPos = 2
-    categoryPos = 3
+    # define column position in case of random header
+    locIdPos, xPos, yPos, categoryPos = 0, 1, 2, 3
+    headerPos = []
     for index in range(4):
         if header[index].lower() == "locid":
-            locIdPos = index
-            break
+            headerPos.append(index)
     for index in range(4):
         if header[index].lower() == "latitude":
-            xPos = index
-            break
+            headerPos.append(index)
     for index in range(4):
-        if header[index][:-1].lower() == "longitude":
-            yPos = index
-            break
+        if header[index].lower() == "longitude":
+            headerPos.append(index)
     for index in range(4):
         if header[index].lower() == "category":
-            categoryPos = index
-            break
+            headerPos.append(index)
+    locIdPos = headerPos[0]
+    xPos = headerPos[1]
+    yPos = headerPos[2]
+    categoryPos = headerPos[3]
 
-    # compare the locId input with locId in locationList to add x, y, category of that locId to a list
-    def compareLoc(locId):
+    # compare the locId input with locId in locationList to add x, y, category element of that locId to a list
+    def element(locId):
         outputList = []
         for location in locationList:
             if locId.lower() == location[locIdPos].lower():
-                outputList.append(float(location[xPos]))
-                outputList.append(float(location[yPos]))
+                try:
+                    outputList.append(float(location[xPos]))
+                    outputList.append(float(location[yPos]))
+                except:
+                    continue
                 outputList.append(location[categoryPos].lower())
         return outputList
 
     # process raw parameters to handle exceptions
-    x1 = compareLoc(queryLocId)[0]
-    y1 = compareLoc(queryLocId)[1]
-    mainCategory = compareLoc(queryLocId)[2]
-    d1 = float(d1)
-    d2 = float(d2)
+    error = False
+    if int(queryLocId[1:]) > len(locationList):
+        print("invalid ID")
+        error = True
+    else:
+        x1 = element(queryLocId)[0]
+        y1 = element(queryLocId)[1]
+        mainCategory = element(queryLocId)[2]
+        d1 = float(d1)
+        d2 = float(d2)
 
     # define whether a specific location is in the area
     def isInArea(x1, x2, y1, y2, d1, d2):
         return x1 - d1 < x2 < x1 + d1 and y1 - d2 < y2 < y1 + d2
 
     # handle duplicated queryLocId and missing data
-    def inCorretedData(location):
-        return location[locIdPos].lower() == queryLocId.lower() or location[locIdPos][1:] == '' or location[locIdPos] == '' or location[xPos] == '' or location[yPos] == '' or location[categoryPos] == '' or location[locIdPos].lower() == "n/a"  or location[xPos].lower() == "n/a" or location[yPos].lower() == "n/a" or location[categoryPos].lower() == "n/a"
+    def incorretedData(location):
+        locId = location[locIdPos].strip().lower()
+        x = location[xPos].strip().lower()
+        y = location[yPos].strip().lower()
+        category = location[categoryPos].strip().lower()
+        return locId == queryLocId.lower() or locId[1:] == "" or locId == '' or x == '' or y == '' or category == '' or locId == "n/a"  or x == "n/a" or y == "n/a" or category == "n/a"
 
     # return locList list
     locList = []
     def locListFunc():
         locListStrip = []
         for location in locationList:
-            if inCorretedData(location):
+            if incorretedData(location):
                 continue
-            x2 = float(location[xPos])
-            y2 = float(location[yPos])
+            try:
+                x2 = float(location[xPos])
+                y2 = float(location[yPos])
+            except:
+                continue
             if isInArea(x1, x2, y1, y2, d1, d2):
                 locList.append(location[locIdPos].upper()) 
         # output strip list
@@ -85,7 +97,7 @@ def main(inputFile,queryLocId,d1,d2):
     def simLocListFunc():
         simLocListStrip = []
         for locId in locList:
-            if compareLoc(locId)[2].lower().strip() == mainCategory:
+            if element(locId)[2].lower().strip() == mainCategory:
                 simLocList.append(locId)
         # output strip list
         for locId in simLocList:
@@ -94,14 +106,15 @@ def main(inputFile,queryLocId,d1,d2):
     
     # return the distance to main location point
     def distanceFunc(x1, x2, y1, y2):
-        return round(((((x2 - x1)**2) + (y2 - y1)**2)) ** (1/2), 4)
+        distance = round(((((x2 - x1)**2) + (y2 - y1)**2)) ** (1/2), 4)
+        return distance
 
     # return distSorted list
     distSorted = []
     def distSortedFunc():
         for locId in simLocList:
-            x2  = compareLoc(locId)[0]
-            y2  = compareLoc(locId)[1]
+            x2  = element(locId)[0]
+            y2  = element(locId)[1]
             distance = distanceFunc(x1, x2, y1, y2)
             distSorted.append(distance)
         return sorted(distSorted)
@@ -110,36 +123,29 @@ def main(inputFile,queryLocId,d1,d2):
     def average(input):
         if input == 0 or len(input) == 0:
             return 0
-        return round(sum(input) / len(input), 4)
+        avg = round(sum(input) / len(input), 4)
+        return avg
 
     # calculate the standard deviation
     def standardDeviation(input):
+        accumulate = 0
         if input == 0 or len(input) == 0:
             return 0
-        accumulate = 0
         for element in input:
             accumulate += (element - average(input)) ** 2
-        return round((accumulate / len(input)) ** (1/2), 4)
+        std = round((accumulate / len(input)) ** (1/2), 4)
+        return std
     
     # return avgstd list
     avgstd = []
     def avgstdFunc():
-        distSorted = distSortedFunc()
         avg = average(distSorted)
         std = standardDeviation(distSorted)
         avgstd.append(avg)
         avgstd.append(std)
         return avgstd
 
-    return locListFunc(), simLocListFunc(), distSortedFunc(), avgstdFunc()
-
-# IMPORTANT: handle exeptions, also delete print(), main()
-# NEED TO REMOVE 
-locList, simLocList, distSorted, avgstd = main("/Users/khanhhuaquang/OneDrive - The University of Western Australia/UWA Learning/Semester 2/CITS1401/Project 1/Locations-sample-Project1 copy.csv", "L83", "1.5", 2.2)
-print(locList)
-print(simLocList)
-print(distSorted)
-print(avgstd)
-# NEED TO REMOVE
-
-
+    if error == True:
+        return [], [], [], [0,0]
+    else:
+        return locListFunc(), simLocListFunc(), distSortedFunc(), avgstdFunc()
